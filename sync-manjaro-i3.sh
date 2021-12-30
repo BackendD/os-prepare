119d9dff634a3949bc417f3bd61e74817db5fb63
BackendD <hasankarimi.dev@gmail.com>
2021-12-30 17:07:27 +0000

    Deploying to gh-pages from @ BackendD/os-prepare@caa5ee7581ed007c21348bf55fb9bfd86f8a1373 🚀

--- sync-manjaro-i3.sh
+++ sync-manjaro-i3.sh
@@ -4,3 +4,7 @@ rclone config create google drive;
 
 ## Shell config
 rclone sync ~/.zshrc google:home/hasan/;
+rclone sync ~/.zsh_history google:home/hasan/;
+
+## SSH
+rclone sync ~/.ssh google:home/hasan/.ssh;
